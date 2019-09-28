1f4d663eb3e198471105b5162b48a44ebdfa187e
Your Name <you@example.com>
2019-09-28 13:00:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/film/Main_title_bar.java
@@ -0,0 +1,14 @@
+package com.example.film;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main_title_bar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_title_bar);
+    }
+}
